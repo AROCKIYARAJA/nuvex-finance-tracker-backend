1e2e5e02bacb6bde2bd7e37d8140fed404bb4a75
arockiyaraja <arockiyaraja88@gmail.com>
2026-04-17 23:50:45 +0530

    key directly added

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect("mongodb+srv://admin:kk9jvkcmcq@arockiyaraja.6ottvpg.mongodb.net/?appName=arockiyaraja");
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
     process.exit(1);
