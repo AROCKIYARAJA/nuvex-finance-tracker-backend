20b033c8a3a0f9b28805f800bdb76bc7fd07ca8c
arockiyaraja <arockiyaraja88@gmail.com>
2026-04-17 23:54:21 +0530

    env key test

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGODB_URI)
     const conn = await mongoose.connect("mongodb+srv://admin:kk9jvkcmcq@arockiyaraja.6ottvpg.mongodb.net/?appName=arockiyaraja");
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
