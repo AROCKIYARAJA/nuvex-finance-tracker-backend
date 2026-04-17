6d1771ca6643097ad5e478831be2b83fc4f4b29d
arockiyaraja <arockiyaraja88@gmail.com>
2026-04-18 00:07:15 +0530

    1. success msg added for indication 2. .env variable added for security purpose

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("env ", process.env.MONGODB_URI)
-    const conn = await mongoose.connect("mongodb+srv://admin:kk9jvkcmcq@arockiyaraja.6ottvpg.mongodb.net/?appName=arockiyaraja");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
     process.exit(1);
