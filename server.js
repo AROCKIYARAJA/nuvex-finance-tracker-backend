5a20d01f8c662360f150e93d6463f4d5812dc911
arockiyaraja <arockiyaraja88@gmail.com>
2026-04-22 20:38:21 +0530

    env variable name is updated correctly

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/nuvex")
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/nuvex")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB error:", err));
 
